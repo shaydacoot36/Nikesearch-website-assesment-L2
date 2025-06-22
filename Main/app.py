96d384dcc1e9713c2950857d99f97b98fd0b0796
shaydacoot36 <kueegrl@gmail.com>
2025-06-23 09:23:38 +1200

    update

--- Main/app.py
+++ Main/app.py
@@ -26,4 +26,13 @@ def index():
     db = get_db()
     shoes = db.execute('SELECT * FROM shoes').fetchall()
     return render_template('home.html', shoes=shoes)
-    
+
+
+@app.route("/bikes")
+def bikes():
+    db = get_db()
+    bikes = db.execute('SELECT * FROM bikes').fetchall()
+    return render_template('bike.html', bikes=bikes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Main/create_shoes_table.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+c.execute('''
+    CREATE TABLE shoes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        size INTEGER NOT NULL,
+        price REAL NOT NULL
+    )
+''')
+conn.commit()
+conn.close()
