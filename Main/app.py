f1eb951abbc3c6674d33b6eb7e7fbb3927f04dbd
shaydacoot36 <kueegrl@gmail.com>
2025-06-23 08:52:28 +1200

    added basic code into important files

--- Main/app.py
+++ Main/app.py
@@ -1,1 +1,29 @@
-print('Hello world')
+from flask import Flask, render_template, g, request, redirect, url_for
+import sqlite3
+import os
+
+
+DATABASE = 'database.db'
+
+app = Flask(__name__)
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    shoes = db.execute('SELECT * FROM shoes').fetchall()
+    return render_template('home.html', shoes=shoes)
+    
