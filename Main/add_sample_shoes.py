7dab99a13234363dce24922d7ae61115cf805524
shaydacoot36 <kueegrl@gmail.com>
2025-07-16 13:12:18 +1200

    I can now see both the shoes on the website

--- Main/add_sample_shoes.py
+++ Main/add_sample_shoes.py
@@ -4,11 +4,11 @@ conn = sqlite3.connect('database.db')
 c = conn.cursor()
 c.execute("""
     INSERT INTO shoes (category_id, model_name, price, release_date, image_url, description)
-    VALUES (1, 'Nike Air', '89.99', '2024-01-01', 'https://example.com/nikeair.jpg', 'Classic Nike Air shoe')
+    VALUES (1, 'Nike Air Max 270', '89.99', '2024-01-01', 'https://example.com/nikeair.jpg', 'Classic Black Nike Air Max 270 shoe')
 """)
 c.execute("""
     INSERT INTO shoes (category_id, model_name, price, release_date, image_url, description)
-    VALUES (2, 'Adidas Runner', '79.99', '2024-02-01', 'https://example.com/adidasrunner.jpg', 'Lightweight Adidas running shoe')
+    VALUES (2, 'Adidas Runner', '79.99', '2024-02-01', 'static/images/nikewhiteshoe.jpg', 'Casual, lightwear streetwear shoe')
 """)
 conn.commit()
 conn.close()
